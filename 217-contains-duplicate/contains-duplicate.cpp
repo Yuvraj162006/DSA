9eb4f7b91c06cfa92bf79e78e3864ae3c57467fc
Yuvraj Singh <ys8556072@gmail.com>
2026-09-06 00:43:09 +0530

    Time: 76 ms (37.25%) | Memory: 113.4 MB (16.03%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
+
+        // TC : O(n)
         unordered_map<int,int>freq;
         for(int x: nums){
             freq[x]++;
